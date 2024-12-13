401bedc16fe8a4e0d8216d2eada748a716beaf3d
mattianatale <mattianatale07@gmail.com>
2024-12-13 16:41:50 +0100

    feat: add javascript istructions

---
+++ js/script.js
@@ -0,0 +1,62 @@
+const teamMembers = [
+    {
+      name: "Marco Bianchi",
+      role: "Designer",
+      email: "marcobianchi@team.com",
+      img: "img/male1.png"
+    },
+    {
+      name: "Laura Rossi",
+      role: "Front-end Developer",
+      email: "laurarossi@team.com",
+      img: "img/female1.png"
+    },
+    {
+      name: "Giorgio Verdi",
+      role: "Back-end Developer",
+      email: "giorgioverdi@team.com",
+      img: "img/male2.png"
+    },
+    {
+      name: "Marta Ipsum",
+      role: "SEO Specialist",
+      email: "martarossi@team.com",
+      img: "img/female2.png"
+    },
+    {
+      name: "Roberto Lorem",
+      role: "SEO Specialist",
+      email: "robertolorem@team.com",
+      img: "img/male3.png"
+    },
+    {
+      name: "Daniela Amet",
+      role: "Analyst",
+      email: "danielaamet@team.com",
+      img: "img/female3.png"
+    }
+  ];
+
+// DOM Elements
+const teamContainerElm = document.querySelector('.team-container')
+
+let listItems = ""
+
+// DOM Events
+for(let i = 0; i < teamMembers.length; i++){
+  const member = teamMembers[i]
+  listItems += `
+  <div class="team-card">
+      <div class="card-image">
+          <img src="${member.img}" alt="${member.name}">
+      </div>
+      <div class="card-description">
+          <h3>${member.name}</h3>
+          <span>${member.role}</span>
+          <span>${member.email}</span>
+      </div>
+  </div>
+  `
+}
+
+teamContainerElm.innerHTML = listItems
